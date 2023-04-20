d4019da4723fceb74081d2c874ea92253919b30a
Elin Robertson <robertsonelin@gmail.com>
2023-04-20 20:04:04 +0200

    added functions for removing product count

--- themes/storefront-child/functions.php
+++ themes/storefront-child/functions.php
@@ -8,6 +8,19 @@ function remove_sidebar() {
 
 add_filter('is_active_sidebar', 'remove_sidebar', 10, 2);
 
+function my_remove_product_result_count() {
+    remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_result_count', 20);
+}
+
+add_action('after_setup_theme', 'my_remove_product_result_count', 99);
+
+
+function my_remove_catalog_ordering() {
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10);
+}
+add_action('after_setup_theme', 'my_remove_catalog_ordering', 99);
+
 
 
 ?>
