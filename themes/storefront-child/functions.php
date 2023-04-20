c47e805bab69b2958e2f653bfd59ac2b5f84f733
Elin Robertson <robertsonelin@gmail.com>
2023-04-20 15:41:47 +0200

    added function for removing sidebar

---
+++ themes/storefront-child/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+remove_action('woocommerce_sidebar', 'remove_sidebar');
+
+function remove_sidebar() {
+    return false;
+}
+
+add_filter('is_active_sidebar', 'remove_sidebar', 10, 2);
+
+
+
+?>
